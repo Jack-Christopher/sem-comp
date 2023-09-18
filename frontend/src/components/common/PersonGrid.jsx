5fdc129b8aef5862f44a93126ec3ea724caeba97
Jack Christopher <jhuaihuah@unsa.edu.pe>
2023-09-18 15:06:33 -0500

    fix: Person grid representation

--- frontend/src/components/common/PersonGrid.jsx
+++ frontend/src/components/common/PersonGrid.jsx
@@ -1,22 +1,24 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Person from "./Person";
 
 export default function PersonGrid({ people }) {
   return (
-    <Grid container columns={{ xs: 4, sm: 8, md: 12 }} colSpacing={{ xs: 0.5, md: 1 }} rowSpacing={2}>
-      {people.map((person, index) => (
-        <Grid item xs={1} sm={2} md={4} key={index}>
-          <Person
-            url={person.url}
-            alt={person.alt}
-            name={person.name}
-            company={person.company}
-            job={person.job}
-            description={person.description}
-            shortDescription={person.shortDescription}
-          />
-        </Grid>              
-      ))}
-    </Grid>
+    <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Grid container spacing={3} >
+        {people.map((person, index) => (
+          <Grid item xs={6} sm={4} md={3} key={index}>
+            <Person
+              url={person.photoUrl}
+              alt={"Foto de " + person.fullname}
+              name={person.fullname}
+              company={person.company}
+              job={person.job}
+              description={person.description}
+              shortDescription={person.shortDescription}
+            />
+          </Grid>              
+        ))}
+      </Grid>
+    </Box>
   )
 }
