7d88c3c860549eae75820381d004df587bae683d
Jack Christopher <jhuaihuah@unsa.edu.pe>
2023-09-18 22:41:52 -0500

    fix: replace Sponsor data from server

--- frontend/src/components/UI/Support.jsx
+++ frontend/src/components/UI/Support.jsx
@@ -1,18 +1,19 @@
+import { useEffect, useState } from "react"
+
 export default function Support() {
-  let data = [
-    {
-      websiteUrl:"https://www.linkedin.com/company/paulonia/?originalSubdomain=pe",
-      photoUrl:"https://semcomp-epcc-unsa.github.io/assets/images/sponsors/paulonia.png",
-    },
-    {
-      websiteUrl:"https://www.ravn.co/",
-      photoUrl:"https://semcomp-epcc-unsa.github.io/assets/images/sponsors/ravn.png",
-    }
-  ];
+  const [sponsors, setSponsors] = useState([]);
 
+  useEffect(() => {
+    fetch(process.env.REACT_APP_BACKEND_URL + '/sponsor/get')
+      .then(res => res.json())
+      .then(data => {
+        console.log('saving data', data);
+        setSponsors(data)
+      })
+      .catch(err => console.log(err));
+  }, []);
 
-  data = data.concat(data, data, data);
-
+  
 
   return (
     <div className="flex flex-col justify-center items-center bg-white py-8">
@@ -20,16 +21,18 @@ export default function Support() {
         Con el apoyo de:
       </h1>
       <div className="flex flex-row justify-center items-center px-32 flex-wrap">
-        {data.map((item) => (
-          <a href={item.websiteUrl} target="_blank"
-            className="w-1/6 h-full rounded-sm border-white border-3 shadow-lg p-4" 
-          >
-            <img 
-              src={item.photoUrl} alt="logo" 
-            />
-          </a>
+        {sponsors.map((item) => (
+          <div key={item.name}>
+            <a href={item.websiteUrl} target="_blank" rel="noreferrer"
+              className="w-1/6 h-full rounded-sm border-white border-3 shadow-lg p-4" 
+            >
+              <img 
+                src={item.photoUrl} alt={item.name} className="h-16 px-3"
+              />
+            </a>
+          </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
